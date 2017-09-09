af99b23be3d9a71b02e48e712a231c84b7830028
AntonyPH <l7tu4ka@gmail.com>
2017-09-09 13:22:10 +0300

    Test php loop in home_gal

--- codeigniter/application/views/pages/home_gal.php
+++ codeigniter/application/views/pages/home_gal.php
@@ -1,40 +1,16 @@
  <div class="col-lg-12">
         <h1 class="page-header">Recent Gallery</h1>
 
-		<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <h4>Album Title</h4>
-            <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="This is my title" data-caption="Some lovely red flowers" data-image="http://#" data-target="#image-gallery">
-                <img class="img-responsive" src="http://fakeimg.pl/365x365/" alt="Short alt text">
-            </a>
-			<div class="gal_menu">
-			<button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal1">VIEW ALBUM</button>
-			</div>
-        </div>
-		<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <h4>Album Title</h4>
-            ><a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="The car i dream about" data-caption="If you sponsor me, I can drive this car" data-image="http://#" data-target="#image-gallery">
-                <img class="img-responsive" src="http://fakeimg.pl/365x365/" alt="A alt text">
-            </a>
-			<div class="gal_menu">
-			<button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal2">VIEW ALBUM</button>
-			</div>
-        </div>
-		<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <h4>Album Title</h4>
-            <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice" data-caption="And if there is money left, my girlfriend will receive this car" data-image="http://#" data-target="#image-gallery">
-                <img class="img-responsive" src="http://fakeimg.pl/365x365/" alt="Another alt text">
-            </a>
-			<div class="gal_menu">
-			<button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal3">VIEW ALBUM</button>
-			</div>
-        </div>
-		<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-      <h4>Album Title</h4>
-            <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice" data-caption="And if there is money left, my girlfriend will receive this car" data-image="http://#" data-target="#image-gallery">
-                <img class="img-responsive" src="http://fakeimg.pl/365x365/" alt="Another alt text">
-            </a>
-			<div class="gal_menu">
-			<button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal4">VIEW ALBUM</button>
-			</div>
-        </div>
+    <?php for($i = 1; $i < 4; $i++)
+    {?>
+      <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <h4>Album Title</h4>
+        <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="This is my title" data-caption="Some lovely red flowers" data-image="http://#" data-target="#image-gallery">
+            <img class="img-responsive" src="http://fakeimg.pl/365x365/" alt="Short alt text">
+        </a>
+  			<div class="gal_menu">
+  			<button type="button" class="btn btn-info" data-toggle="modal" data-target=<?php"#myModal".$i;?>>VIEW ALBUM</button>
+  			</div>
+      </div>
+<?php}?>
 	</div>
